3f0f61fc86bc194b1c1c71cbb3efa96ea86cb9dc
abisheik.r <abisheik.r@sporfy.com>
2024-03-09 17:30:23 +0530

    project list

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,10 @@
+import ProjectCard from "@/components/projectCard/projectCard";
+
+export default function Projects() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>Comming soon own portfolio</div>
+      <ProjectCard/>
+    </main>
+  );
+}
---
+++ src/components/projectCard/projectCard.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function ProjectCard() {
+  return (
+    <main className="p-24">
+<div>
+  Comming soon own portfolio
+</div>
+
+    </main>
+  );
+}
